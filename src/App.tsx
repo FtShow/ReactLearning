af5ee1ed7d60454fe9784303de8d58093aab3c25
dmitry_i <fstpain@gmail.com>
2023-05-14 20:03:54 +0300

    add reactmemo2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
     const [onOfValue, setOnOfValue] = useState(true);
     const onChangeHandler = () => {
         setOnOfValue(!onOfValue)
+        console.log('ree')
     }
     const changeRating = (v:ratingValueType)=>{
         setRatingValue(v)
